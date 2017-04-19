4f2b404c59a07349fc5ea2f756eff5d2a9cf1ed1
Ryan Goldstein <ryanisaacg@gmail.com>
2017-04-19 15:52:54 -0400

    Added a quick test of the map to the main file

--- source/app.d
+++ source/app.d
@@ -4,8 +4,9 @@ import geom;
 
 void main()
 {
-	auto vec = Vector2(5,5);
 	Tilemap!int map = Tilemap!int(640, 480, 32, 32);
 	map.set(5, 5, 1);
-	writeln(map.get(6, 33));
+	auto player = Rectangle(30, 38, 4, 4);
+	auto vec = Vector2(-1, -10);
+	writeln(map.slideMove(player, vec));
 }
